d19b501f1318dc76790dfec8be9bacfe77e71011
tiago <tiagopinto.dev@gmail.com>
2022-09-10 13:21:22 +0100

    proxy cors changed

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
         context:['/api'],
-        target: 'http://localhost:8080/',
+        target: 'https://papiro-spring-api.herokuapp.com/',
         secure:false,
         logLevel : 'debug'
     }
